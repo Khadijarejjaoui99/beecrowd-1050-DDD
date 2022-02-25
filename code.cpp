3b94a9f7dfc99b8b5cca6754b182f13047df1ca2
FIRST_NAME LAST_NAME <khadijarejjaoui41@gmail.com>
2022-02-26 02:57:45 +0600

    first commit

---
+++ code.cpp
@@ -0,0 +1,41 @@
+// beecrowd 1050 DDD
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    int number;
+    cin >> number;
+    switch (number)
+    {
+    case 61:
+        cout<<"Brasilia"<<endl;
+        break;
+    case 71:
+        cout<<"Salvador"<<endl;
+        break;
+    case 11:
+        cout<<"Sao Paulo"<<endl;
+        break;
+    case 21:
+        cout<<"Rio de Janeiro"<<endl;
+        break;
+    case 32:
+        cout<<"Juiz de Fora"<<endl;
+        break;
+    case 19:
+        cout<<"Campinas"<<endl;
+        break;
+    case 27:
+        cout<<"Vitoria"<<endl;
+        break;
+    case 31:
+        cout<<"Belo Horizonta"<<endl;
+        break;
+    default:
+        cout<<"DDD nao cadastrado"<<endl;
+        break;
+    }
+    
+    system("pause");
+    return 0;
+}
